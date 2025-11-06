e3453a7664fd9571e5427805ac67245157ee670b
pakalagovardhan <p.govardhan321@gmail.com>
2025-11-06 18:08:37 +0530

    datatypes

---
+++ 02_datatypes.py
@@ -0,0 +1,11 @@
+test_list = ["Hello", "world", "python"]
+print(test_list)
+test_tuple = ("Hello", "world", "python")
+print(test_tuple)
+test_dict = {'a': 1, 'b': 2}
+print(test_dict)
+test_set = {'a', 'b', "abc"}
+print(test_set)
+
+print(type(test_set))
+print(type(print))
